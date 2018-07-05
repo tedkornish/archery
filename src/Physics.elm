7a9e47a9e91211ed2098f7bf55ca6e1362822481
Ted Kornish <ted@tedkornish.com>
2018-07-05 15:44:40 -0700

    Add acceleration

--- src/Physics.elm
+++ src/Physics.elm
@@ -1,20 +1,33 @@
-module Physics exposing (tick)
+module Physics
+    exposing
+        ( tick
+        , stillObject
+        , Object
+        , Coordinates
+        )
 
 
 type alias Object =
-    { pos : Position, vel : Velocities }
+    { pos : Coordinates, vel : Coordinates, accel : Coordinates }
 
 
-type alias Position =
+type alias Coordinates =
     { x : Int, y : Int }
 
 
-type alias Velocities =
-    { x : Int, y : Int }
+emptyCoords : Coordinates
+emptyCoords =
+    { x = 0, y = 0 }
+
+
+stillObject : Coordinates -> Object
+stillObject startingCoords =
+    { pos = startingCoords, vel = emptyCoords, accel = emptyCoords }
 
 
 tick : Object -> Object
 tick obj =
     { obj
         | pos = { x = obj.pos.x + obj.vel.x, y = obj.pos.y + obj.vel.y }
+        , vel = { x = obj.vel.x + obj.accel.x, y = obj.vel.y + obj.accel.y }
     }
--- tests/PhysicsTest.elm
+++ tests/PhysicsTest.elm
@@ -3,25 +3,29 @@ module PhysicsTest exposing (..)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
-import Physics exposing (tick)
+import Physics exposing (tick, stillObject)
 
 
 suite : Test
 suite =
     describe "The Physics module"
         [ describe "Physics.tick"
-            [ test "position stays the same for an object with no momentum" <|
+            [ test "position stays the same for an object with no velocity or acceleration" <|
                 \_ ->
                     let
-                        emptyVelocities =
-                            { x = 0, y = 0 }
-
                         pos =
                             { x = 3, y = 4 }
 
                         startingObject =
-                            { pos = pos, vel = emptyVelocities }
+                            stillObject pos
                     in
                         Expect.equal (tick startingObject) startingObject
+            , test "y momentum falls for an object under gravity" <|
+                \_ ->
+                    let
+                        startingObject =
+                            { pos = { x = 3, y = 4 }, vel = { x = 0, y = 0 }, accel = { x = 0, y = -1 } }
+                    in
+                        Expect.equal (tick startingObject) ({ startingObject | vel = { x = 0, y = -1 } })
             ]
         ]
