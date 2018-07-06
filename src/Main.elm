272a17457ac0183435337a54e14e0fe90e066bf4
Ted Kornish <ted@tedkornish.com>
2018-07-05 20:05:28 -0700

    Let objects go above the screen

--- src/Main.elm
+++ src/Main.elm
@@ -75,10 +75,7 @@ type Msg
 
 inBounds : Window.Size -> Object -> Bool
 inBounds size obj =
-    (obj.pos.x > 0)
-        && (obj.pos.y > 0)
-        && (obj.pos.x < (toFloat size.width))
-        && (obj.pos.y < (toFloat size.height))
+    (obj.pos.x < (toFloat size.width)) && (obj.pos.y < (toFloat size.height))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
